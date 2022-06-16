a3b0546cacbe5fee705f0bc48a213a17b97d4ebd
abhishekfegno <abhishek@fegno.com>
2022-06-16 18:29:50 +0530

    category api

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -33,7 +33,7 @@ class PDF(models.Model):
     slug = AutoSlugField(max_length=50, populate_from='title', null=True, blank=True)
     file = models.FileField(upload_to='pdf/product/', blank=True, null=True)
     is_public = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=False, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='pdf', blank=False, null=True)
 
     def __str__(self):
         return self.category.name
