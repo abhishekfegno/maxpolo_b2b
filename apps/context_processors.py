6b58dbef120959e9740fb8eb6072e84af0e47bd8
abhishekfegno <abhishek@fegno.com>
2022-06-16 11:54:29 +0530

    user role

--- apps/context_processors.py
+++ apps/context_processors.py
@@ -2,9 +2,11 @@ from apps.user.models import SiteConfiguration
 
 
 def settings(request):
+    data = {}
     from django.conf import settings
+    logo = SiteConfiguration.objects.first().site_logo
+    data['settings'] = settings
+    if logo:
+        data[logo] = logo.url
 
-    data = {'settings': settings,
-            'logo': SiteConfiguration.objects.first().site_logo.url
-            }
     return data
