04a26716e2a4287304eda5dc35e5cb55f328f527
abhishekfegno <abhishek@fegno.com>
2022-06-17 15:21:37 +0530

    order create api

--- apps/order/api/urls.py
+++ apps/order/api/urls.py
@@ -2,9 +2,9 @@
 from django.urls import path, include
 
 from apps.catalogue.api.viewsets import ProductAPIView
-from apps.order.api.viewsets import OrderSerializerAPIView
+from apps.order.api.viewsets import OrderListAPIView
 
 app_name = 'order'
 urlpatterns = [
-    path('order/list/', OrderSerializerAPIView.as_view(), name='api-order-list'),
+    path('order/list/', OrderListAPIView.as_view(), name='api-order-list'),
 ]
--- apps/order/api/viewsets.py
+++ apps/order/api/viewsets.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator, EmptyPage
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
@@ -9,16 +10,17 @@ from apps.catalogue.api.serializers import ProductSerializer
 from apps.catalogue.models import Product
 from apps.order.api.serializers import OrderSerializer
 from apps.order.models import SalesOrder, SalesOrderLine
+from apps.user.models import Dealer
 from lib.utils import list_api_formatter
 
 
-class OrderSerializerAPIView(ListAPIView):
+class OrderListAPIView(ListAPIView):
     """
     POST data
     {
         "products":[1,2,3],
         "quantites":[1,2,3],
-        "dealer":1
+        "dealer":2
     }
 
     """
@@ -44,15 +46,17 @@ class OrderSerializerAPIView(ListAPIView):
         return Response(list_api_formatter(request, paginator=paginator, page_obj=page_obj, results=serializer.data))
 
     def post(self, request, *args, **kwargs):
+        result = {}
+        dealer = request.data.get('dealer')
         quantity = request.data.get('products')
         products = request.data.get('quantites')
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            # import pdb;pdb.set_trace()
-            order = serializer.save()
+        try:
+            order = SalesOrder.objects.create(dealer=Dealer.objects.get(id=dealer))
             for product, quantity in zip(products, quantity):
                 line = SalesOrderLine.objects.create(product=Product.objects.get(id=product), quantity=quantity,
                                                      order=order)
                 print(f"line created {line} for order {order}")
-
-        return Response()
+        except Exception as e:
+            result['errors'] = str(e)
+        return Response(result, status=status.HTTP_200_OK)
