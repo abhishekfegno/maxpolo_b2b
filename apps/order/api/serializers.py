376aee825af0e8efc2e34ab0f6c1dd10a0128064
abhishekfegno <abhishek@fegno.com>
2022-06-24 14:52:15 +0530

    asdf

--- apps/order/api/serializers.py
+++ apps/order/api/serializers.py
@@ -41,3 +41,25 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ('id', 'order_id', 'invoice_id', 'invoice_status', 'invoice_date', 'invoice_amount',
                   'invoice_remaining_amount', 'confirmed_date', 'is_invoice', 'is_cancelled', 'is_confirmed',
                   'is_quotation', 'dealer', 'created_at', 'dealer', 'line')
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    line = OrderLineSerializer(many=True)
+    dealer = serializers.SerializerMethodField()
+    transactions = serializers.SerializerMethodField()
+
+    def get_dealer(self, instance):
+        if instance.dealer:
+            return {
+                "id": instance.dealer_id,
+                "name": instance.dealer.get_full_name()
+            }
+
+    def get_transactions(self, instance):
+        return instance.transaction_set.all().values('amount', 'created_at')
+
+    class Meta:
+        model = SalesOrder
+        fields = ('id', 'order_id', 'invoice_id', 'invoice_status', 'invoice_date', 'invoice_amount',
+                  'invoice_remaining_amount', 'confirmed_date', 'is_invoice', 'is_cancelled', 'is_confirmed',
+                  'is_quotation', 'dealer', 'created_at', 'dealer', 'line', 'transactions')
