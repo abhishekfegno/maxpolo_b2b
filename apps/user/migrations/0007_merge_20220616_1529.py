70c64edbebedce16da96acaec964d606ec2af249
abhishekfegno <abhishek@fegno.com>
2022-07-05 11:32:05 +0530

    category api

--- apps/user/migrations/0007_merge_20220616_1529.py
+++ apps/user/migrations/0007_merge_20220616_1529.py
@@ -6,9 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0006_merge_20220616_1236'),
-        ('user', '0006_merge_20220616_1428'),
+        # ('user', '0006_merge_20220616_1236'),
+        # ('user', '0006_merge_20220616_1428'),
+        ('user', '0006_auto_20220616_2359'),
+
     ]
+
 
     operations = [
     ]
