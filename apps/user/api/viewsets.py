81baeecb14c47eaf404f59ad8cbb846973266e93
abhi44 <raviabhishek445@gmail.com>
2022-07-14 13:52:09 +0530

    token authentication

--- apps/user/api/viewsets.py
+++ apps/user/api/viewsets.py
@@ -214,7 +214,7 @@ class ProfileAPIView(ExeDealerMixin, GenericAPIView):
             result = {str(e)}
         return Response(result)
 
-    def put(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         result = {}
         serializer = self.get_serializer(request.user, data=request.data)
         if serializer.is_valid():
